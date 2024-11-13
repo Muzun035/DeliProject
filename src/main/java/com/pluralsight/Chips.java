535228dfb9e2513018cc0cd9f0fc7563285d659f
Muzun <muzun@pgh.yearup.org>
2024-11-13 09:01:37 -0500

    Chips class file added

---
+++ src/main/java/com/pluralsight/Chips.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class Chips implements IPriceable {
+    private String type;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public double calculatePrice() {
+        return 1.50;
+    }
+}
